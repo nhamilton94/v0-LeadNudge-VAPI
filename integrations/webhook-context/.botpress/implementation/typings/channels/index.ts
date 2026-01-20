d852fcdcd81fc2cb41c9afacb8e53ff29f9ad9f3
nhamilton94 <156883584+nhamilton94@users.noreply.github.com>
2026-01-19 20:06:32 -0500

    Delete integrations directory

--- integrations/webhook-context/.botpress/implementation/typings/channels/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// This file is generated. Do not edit it manually.
-import * as webhook from "./webhook/index";
-export * as webhook from "./webhook/index";
-
-export type Channels = {
-  "webhook": webhook.Webhook;
-}
--- integrations/webhook-context/.botpress/implementation/typings/states/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// This file is generated. Do not edit it manually.
-import * as contactContext from "./contactContext/index";
-export * as contactContext from "./contactContext/index";
-
-export type States = {
-  "contactContext": contactContext.ContactContext;
-}
